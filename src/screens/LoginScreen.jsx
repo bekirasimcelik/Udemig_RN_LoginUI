3cdf871bd791bbf5cdce86343f5f57b4a6296eb5
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-08-05 23:29:02 +0300

    loginScreen updated

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,10 +1,14 @@
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView, TouchableOpacity} from 'react-native';
 import React from 'react';
 
 export default function LoginScreen() {
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View className="flex-1 bg-white" style={{backgroundColor: '#9370db'}}>
+      <SafeAreaView className="flex">
+        <View className="flex-row justify-start">
+          <TouchableOpacity className=""></TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
