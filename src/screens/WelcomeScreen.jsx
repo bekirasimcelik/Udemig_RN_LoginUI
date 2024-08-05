0e60ec050dd5c83a37ec12cd93a5e42230055107
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-08-05 22:48:51 +0300

    welcomeScreen updated

--- src/screens/WelcomeScreen.jsx
+++ src/screens/WelcomeScreen.jsx
@@ -1,11 +1,38 @@
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
+import {useNavigation} from '@react-navigation/native';
 
 export default function WelcomeScreen() {
+  const navigation = useNavigation();
   return (
     <SafeAreaView className="flex-1" style={{backgroundColor: '#9370db'}}>
       <View className="flex-1 flex justify-around my-4">
-        <Text>Let's Get Started!</Text>
+        <Text className="text-white font-bold text-4xl text-center">
+          Let's Get Started!
+        </Text>
+        <View className="flex-1 justify-center">
+          <Image
+            source={require('../assets/welcome.png')}
+            style={{width: 350, height: 350}}
+          />
+        </View>
+        <View className="space-y-4">
+          <TouchableOpacity
+            onPress={() => navigation.navigate('SignUp')}
+            className="py-3 bg-yellow-400 mx-7 rounded-xl">
+            <Text className="text-xl font-bold text-center text-gray-700">
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+          <View className="flex-row justify-center">
+            <Text className="text-white font-semibold">
+              Already have an account?
+            </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+              <Text className="font-semibold text-yellow-400">Log In</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
