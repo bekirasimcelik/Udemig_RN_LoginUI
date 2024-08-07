acf7311c3bde64956b3f26d46faf804467be3152
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-08-07 21:47:00 +0300

    react-native-svg implemented for icons

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,14 +1,14 @@
 import {View, Text, SafeAreaView, TouchableOpacity} from 'react-native';
 import React from 'react';
-import {ArrowLoeftIcon} from 'react-native-heroicons/solid';
+import {ArrowLeftIcon} from 'react-native-heroicons/solid';
 
 export default function LoginScreen() {
   return (
     <View className="flex-1 bg-white" style={{backgroundColor: '#9370db'}}>
       <SafeAreaView className="flex">
         <View className="flex-row justify-start">
-          <TouchableOpacity className="bg-yellow-400 rounded-tr-2xl p-2 rounded">
-            <ArrowLoeftIcon size="20" color="black" />
+          <TouchableOpacity className="bg-yellow-400 rounded-tr-2xl p-2 rounded-bl-2xl">
+            <ArrowLeftIcon size="20" color="black" />
           </TouchableOpacity>
         </View>
       </SafeAreaView>
